5ca2dd7a5774940249da09de8df00f33c5b6f313
Larissa Bicalho <lari_nazarebicalho@hotmail.com>
2019-04-25 11:02:27 -0300

    Finalizada Prática

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,17 @@
 // Faça o exercício dos parágrafos aqui
 // Este arquivo ainda não está incluído no arquivo HTML
+
+let botões = document.querySelectorAll('.botao-expandir-retrair');
+botões.forEach(botão =>{
+  botão.addEventListener('click',function(event){
+     let choosenB = event.currentTarget;
+     let chossenParent = choosenB.parentElement;
+     chossenParent.classList.toggle('expandido');
+     if(choosenB.innerHTML ==="+") 
+          choosenB.innerHTML ='-';
+        else {
+            choosenB.innerHTML="+";
+        }
+    
+  });
+});
--- exercicio2.js
+++ exercicio2.js
@@ -9,3 +9,30 @@ const servidorDasImagens = 'https://fegemo.github.io/cefet-web/images/',
     'philae-67-picture.jpg',
     'philae-collecting.jpg'
   ];
+
+  let index = 0;
+
+  let img_first = document.querySelector('#slide');
+
+  let botao_prox = document.querySelector('#proximo');
+
+  botao_prox.addEventListener('click', function(){
+    index = (index + 1) % todasAsImagens.length;
+    img_first.src = servidorDasImagens + todasAsImagens[index];
+
+  });
+
+
+let botao_ant = document.querySelector('#anterior');
+
+botao_ant.addEventListener('click', function(){
+  index--;
+
+  if(index < 0){
+      index = todasAsImagens.length-1;
+  }else{
+      index %=todasAsImagens.length;  
+  }
+  img_first.src = servidorDasImagens + todasAsImagens[index];  
+
+});
